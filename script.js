111f210c040601097780cff22d02fc229237e9d1
Shane Prendergast <contact@webknit.co.uk>
2014-08-19 17:10:26 +0100

    adding base JS functionality

--- script.js
+++ script.js
@@ -10,35 +10,51 @@
 
 var Base = Base || {};
 
-Base.functionName = function()
-{	
-	var self = $(this);
-	var variable = $('.var');
+// Parallax actions
+Base.ParallaxFunction = function() {
+	
+	// Declare window inside a variable
+	var $window = $(window);
 
-	function init()
-	{
-		variable.click(functionOne);
-		functionTwo();
+	// Call functions
+	function init() {
+	
+		// Reuseable functions activates each parallax section
+		//
+		// Call parallaxIt passing two variables
+		// parallaxIt(CONTAINER, CONTAINER PARALLAX IMG );
+		// eg parallaxIt($(.container).scrollTop(), $(.container img));
+		// The .scrollTop() calculates from the top of the page.
+		// We also subtract how far we are from the top of the page 
+		// from how far the element is from the top of the page
+		// parallaxIt($(.container).offset().top - $window.scrollTop(), $(.container img));
+		$window.scroll(function(e) {
+		
+			parallaxIt($('.img-section-zero').offset().top - $window.scrollTop(), $('.img-zero'));
+		    parallaxIt($('.img-section-one').offset().top - $window.scrollTop(), $('.img-one'));
+		    parallaxIt($('.img-section-two').offset().top - $window.scrollTop(), $('.img-two'));
+		    parallaxIt($('.img-section-three').offset().top - $window.scrollTop(), $('.img-three'));
+		    parallaxIt($('.img-section-four').offset().top - $window.scrollTop(), $('.img-four'));
+		    
+		});
+		
 	}
 
-	function functionOne()
-	{
-		// JS CODE
-	}
-
-	function functionTwo()
-	{
-		// JS CODE
-		window.hide();
+	function parallaxIt(containerElement, imgName) {
+	
+		var scrolled = containerElement;
+		imgName.css('top', -(scrolled * 0.5) + 'px');
+		
 	}
 
 	init();
+	
 };
 
 // ON DOC READY
 $(function()
 {	
-	new Base.functionName();
+	new Base.ParallaxFunction();
 	
 });
 
